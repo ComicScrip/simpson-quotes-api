051e2e44313b874c570a0afee690546eadcb33e9
Pierre Genthon <pierregenthon.pro@gmail.com>
2020-02-14 18:53:00 +0100

    4 random quotes

--- express/server.js
+++ express/server.js
@@ -5,10 +5,41 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
+const quotes = [
+  {
+    quote:
+      "Facts are meaningless. You could use facts to prove anything that's even remotely true.",
+    character: "Homer Simpson",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+  },
+  {
+    quote: "Nothing you say can upset us. We're the MTV generation.",
+    character: "Bart Simpson",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FBartSimpson.png?1497567511638"
+  },
+  {
+    quote: "That's where I saw the leprechaun...He told me to burn things.",
+    character: "Ralph Wiggum",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
+  },
+  {
+    quote:
+      "Hello, Simpson. I'm riding the bus today because Mother hid my car keys to punish me for talking to a woman on the phone. She was right to do it.",
+    character: "Principal Skinner",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FSeymourSkinner.png?1497567511460"
+  }
+];
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.json({test: 'ok'})
-});
+router.get('/', (req, res) => res.json(quotes[randomInteger(0, quotes.length - 1)]));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
